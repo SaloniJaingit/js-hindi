9751b826534e7047f72f4b8c37d36261ade21982
SaloniJaingit <salonijain1598@gmail.com>
2024-10-18 15:13:28 +0000

    our new thing done js

--- basic-js/variable.js
+++ basic-js/variable.js
@@ -3,4 +3,17 @@ let accountEmail = "saloni@gmail.com";
 var accountPassword = "1234";
 accountCity = "jaipur";
 
+let accountState;
+
 // accountId = 2;
+
+accountEmail = "shelu@gamil.com";
+
+accountPassword = "3421";
+
+accountCity = "bangaluru";
+
+/* Prefer not use var because of functional and blok scope  */
+
+console.table([accountEmail, accountPassword, accountCity, accountState]);
+
