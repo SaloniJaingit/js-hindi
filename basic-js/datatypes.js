c34be48c16daf9895bb40519fffc6e2c81e1decb
SaloniJaingit <salonijain1598@gmail.com>
2024-10-19 02:26:10 +0000

    datatypes

---
+++ basic-js/datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // Treat js as a new version
+
+// alert(3 + 3); We are using node js not browser
+
+
+let name = "saloni";
+
+let age = 27;
+
+let isLogedIn = false;
+
+// number =>2 to power 53
+
+// bigint
+
+// string => ""
+
+// boolean => true, false
+
+// null => standlone value
+
+// undefine
