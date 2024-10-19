ec2f5f3a3244150bd0379fd0fbfe468a0a2d3cef
SaloniJaingit <salonijain1598@gmail.com>
2024-10-19 02:30:34 +0000

    datatype

--- basic-js/datatypes.js
+++ basic-js/datatypes.js
@@ -10,13 +10,15 @@ let age = 27;
 let isLogedIn = false;
 
 // number =>2 to power 53
-
 // bigint
-
 // string => ""
-
 // boolean => true, false
-
 // null => standlone value
-
 // undefine
+// symbol => unique
+// object
+
+console.log(typeof "saloni");  // undefine
+
+console.log(typeof null);  // object
+
