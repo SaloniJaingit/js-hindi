7ec37d3e5c60cde162a6852754deda2e6ccde226
SaloniJaingit <salonijain1598@gmail.com>
2024-10-19 03:56:23 +0000

    conversion of data

---
+++ basic-js/conversionoroperations.js
@@ -0,0 +1,43 @@
+// let score = "39abc";
+
+// let score = 33;
+
+// let score = null;
+
+// let score = undefine;
+
+// let score="saloni";
+
+// console.log(typeof score);
+
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN(not a number)
+// true => 1
+// false=> 0
+
+// let isLoggedIN = 1;
+
+// let isLoggedIN = "";
+
+// let isLoggedIN = "saloni";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIN)
+
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 33;
+
+let someNumbers = String(someNumber);
+
+console.log(someNumbers);
+
+console.log(typeof someNumbers);
+
