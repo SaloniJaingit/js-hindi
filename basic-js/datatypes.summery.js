77366ba640d59efc1af3a3d2460e06c5d8901ff6
SaloniJaingit <salonijain1598@gmail.com>
2024-11-26 09:36:40 +0000

    datatype.summery

--- basic-js/datatypes.summery.js
+++ basic-js/datatypes.summery.js
@@ -6,7 +6,23 @@ const score = 100;
 
 const scoreValue = 100.3;
 
+const isLoggedIn = false;
 
+const outSidetemp = null;
+
+let userEmail;
+
+//symbol
+
+const id = Symbol('123')
+
+console.log(id)
+
+const anotherId = Symbol('123')
+
+console.log(anotherId)
+
+console.log(id == anotherId)
 
 // Referance(Non-Premitive): 
 
