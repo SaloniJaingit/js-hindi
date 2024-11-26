3c9bf77233390a22be5e10b688a2914da4afa5d2
SaloniJaingit <salonijain1598@gmail.com>
2024-11-26 09:07:40 +0000

    datatypes.summery

---
+++ basic-js/datatypes.summery.js
@@ -0,0 +1,13 @@
+// Preimitive
+
+// 7 Types: Boolean, Number, String, Null, Undefine, BigInt, Symbol
+
+const score = 100;
+
+const scoreValue = 100.3;
+
+
+
+// Referance(Non-Premitive): 
+
+// Array, Objects, Functions 
