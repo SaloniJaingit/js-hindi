95aa7eba27a460b54c38dbd3eef2a4f1c554a8d1
SaloniJaingit <salonijain1598@gmail.com>
2024-11-30 08:40:27 +0000

    string

---
+++ basic-js/String.js
@@ -0,0 +1,21 @@
+const game = "saloni";
+const newGame = "newBadmen";
+
+// console.log(game + newGame + "hey");
+
+// console.log(`Hello my name is ${game} and my favourite game is ${newGame}`);
+
+const newName = "salonih"
+
+// console.log(newName[4]);
+// console.log(newName.indexOf(4));
+// console.log(newName.charAt('i'));
+
+const mail = "salonijain%20@gamil.com";
+
+// console.log(mail.replace("%20","53"));
+
+const trim = "  trimthis ";
+
+console.log(trim.trim());
+
